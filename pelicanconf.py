923d4b9411ca0e4fb9909a6c93c75cd4332dfd00
Aaron Iles <aaron.iles@gmail.com>
2013-08-09 22:38:34 +1000

    Extend configuration settings
    Expand the number of configuration items set, with a couple of updates.

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,16 +3,19 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'Aaron Iles'
+AUTHOR_EMAIL = 'aaron.iles@gmail.com'
 SITENAME = 'Predictably Random'
 SITEURL = 'http://www.aaroniles.net'
 
 TIMEZONE = 'Australia/Canberra'
 
 DEFAULT_LANG = 'en'
-DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = 3
 
 ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
 ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+
+SUMMARY_MAX_LENGTH = None
 
 # Feed generation is usually not desired when developing
 FEED_DOMAIN = SITEURL
@@ -26,20 +29,25 @@ TRANSLATION_FEED_ATOM = None
 TRANSLATION_FEED_RSS = None
 
 # Blogroll
-LINKS =  (('Python', 'http://python.org/'),
-          ('Pelican', 'http://getpelican.com/'),
-          ('Travis', 'https://travis-ci.org'),
-          ('Heroku', 'https://www.heroku.com'))
+LINKS =  (('python', 'http://python.org'),
+          ('pelican', 'http://getpelican.com'),
+          ('bootstrap', 'http://getbootstrap.com'),
+          ('font awesome', 'http://fortawesome.github.io/'),
+          ('travis', 'https://travis-ci.org'),
+          ('heroku', 'https://www.heroku.com'))
 
 # Social widget
 SOCIAL = (('twitter', 'https://twitter.com/aliles'),
           ('github', 'https://github.com/aliles'),
+#         ('dropbox', 'http://db.tt/76kN97G'),
+#         ('google-plus', 'https://plus.google.com/110197178230796133534'),
           ('linkedin', 'https://www.linkedin.com/in/aliles'))
 
 # Theme
 THEME = 'theme'
-DISQUS_SITENAME = None
-GOOGLE_ANALYTICS = None
+DISQUS_SITENAME = 'aaronilesnet'
+GOOGLE_ANALYTICS = 'UA-43019090-1'
+# GOOGLE_CUSTOM_SEARCH_SIDEBAR = '011005344798390208422'
 GITHUB_URL = 'https://github.com/aliles'
 
 # Uncomment following line if you want document-relative URLs when developing
